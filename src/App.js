28dde72fd2e288ad803277df874914aefd16cde9
mar1n <30738216+mar1n@users.noreply.github.com>
2020-03-15 23:59:52 +0000

    add images

--- src/App.js
+++ src/App.js
@@ -1,32 +1,37 @@
 import React, { useState } from 'react';
 import logo from './logo.svg';
+import part1 from './img/part1.png'
+import part2 from './img/part2.png'
+import part3 from './img/part3.png'
 import './App.css';
 
 function App() {
-   let [number, setNumber]  = useState([
-     1,
-     2,
-     3,
-     4,
-     5
+   let [images, setImages]  = useState([
+     {image: 'part1', active: true},
+     {image: 'part2', active: true},
+     {image: 'part3', active: true}
    ]);
    let [currentNum, setCurrentNum] = useState(0);
    function nextNumber() {
      console.log('next');
-     currentNum === number.length - 1
-     ? setCurrentNum(0)
-     : setCurrentNum(currentNum + 1)
+    //  currentNum === number.length - 1
+    //  ? setCurrentNum(0)
+    //  : setCurrentNum(currentNum + 1)
    }
    function previusNumber() {
      console.log('previus');
-     currentNum === 0
-     ? setCurrentNum(number.length - 1)
-     : setCurrentNum(currentNum - 1)
+    //  currentNum === 0
+    //  ? setCurrentNum(number.length - 1)
+    //  : setCurrentNum(currentNum - 1)
    }
   return (
     <div className="App">
-      <div className="numbers">
-        {number[currentNum]}
+      <div className="carousel">
+        {images.map(image => 
+          <div className="carousel">
+            <img src={require(`./img/${image.image}.png`)} alt={image.name}/>
+          </div>
+        )}
       </div>
       <div className="controllers">
         <button
