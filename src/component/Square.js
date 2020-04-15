504b547893868d4cddb8cd017a8ebda31d03075d
mar1n <30738216+mar1n@users.noreply.github.com>
2020-04-15 22:04:30 +0100

    add module hot

---
+++ src/component/Square.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Square.css';
+
+const Square = () =>
+    <div className='squares'>
+        <div className='square-1'></div>
+        <div className='square-2'></div>
+    </div>
+
+export default Square;
