754741c16d30600f328e242d5b6bc3f54227267b
mar1n <30738216+mar1n@users.noreply.github.com>
2020-04-15 22:48:01 +0100

    CSS squer/containers loop

--- src/component/Square.js
+++ src/component/Square.js
@@ -3,8 +3,8 @@ import './Square.css';
 
 const Square = () =>
     <div className='squares'>
-        <div className='square-1'></div>
-        <div className='square-2'></div>
+        <div className='square-1'>1</div>
+        <div className='square-2'>2</div>
     </div>
 
 export default Square;
